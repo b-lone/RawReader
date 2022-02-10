ed7c3f6adeb179644451e451b4dfd66705662e9a
Archie <youkun@bilibili.com>
2022-02-10 20:09:25 +0800

    Initial Commit

---
+++ RawReader/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RawReader
+//
+//  Created by Archie on 2022/2/10.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
